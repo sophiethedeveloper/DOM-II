6ea93df767adeb9b676e026e53e9f96ff17a9e6b
sofia Luna <Sofia21luna@gmail.com>
2020-09-01 15:21:00 -0400

    Key Down

--- js/index.js
+++ js/index.js
@@ -18,3 +18,10 @@ let adventureImg = document.querySelector('.img-content img')
 adventureImg.addEventListener('dblclick', (event) => {
     event.target.src = "https://images.unsplash.com/photo-1515552726023-7125c8d07fb3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"
 })
+
+//4 -Keydown
+const keyListener = document.querySelector("#subject");
+
+keyListener.addEventListener('keydown', (e) => {
+  keyListener.textContent += `${e.code}`
+})
