618d81e7ac48f0a30d9b486bad1e4eb050eed0a2
sofia Luna <Sofia21luna@gmail.com>
2020-09-01 15:39:46 -0400

    Mouse Over

--- js/index.js
+++ js/index.js
@@ -76,4 +76,32 @@ function zoom(event) {
   let scale = 1;
   const el = document.querySelector('.lastBoat');
   el.onwheel = zoom;
+
   
+//10 - MouseOver
+
+const coloredNav = document.querySelector('.nav')
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+coloredNav.addEventListener("mouseenter", function( event ) {   
+  // highlight the mouseenter target
+  event.target.style.color = "purple";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+coloredNav.addEventListener("mouseover", function( event ) {   
+  // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
