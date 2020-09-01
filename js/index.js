8ff4432447b06084a778a7c1def274a71876c73b
sofia Luna <Sofia21luna@gmail.com>
2020-09-01 15:12:07 -0400

    mouseenter

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// 1 -mouseenter 
+const funBus = document.querySelector('.intro img');
+
+funBus.addEventListener('mouseenter', (e) => {
+    funBus.style.transform = "scale(1.2)"
+    funBus.style.transition = "all 0.3s"
+  })
