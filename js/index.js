d63bd5841a5ffc8e9d2d0eb2a75a79816a17ff73
sofia Luna <Sofia21luna@gmail.com>
2020-09-01 15:23:24 -0400

    Click Event

--- js/index.js
+++ js/index.js
@@ -32,3 +32,10 @@ const focusListener = document.querySelector('input[type=text]');
 focusListener.addEventListener('focus', (event) => {
   event.target.style.background = '#90ee90';
 })
+
+// 6 -Click
+const boatImage = document.querySelector('.inverse-content img');
+
+boatImage.addEventListener('click', (event) => {
+  event.target.src = 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+})
