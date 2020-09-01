7ff841b90b09041bdf3747fe7ffe3c0a15353750
sofia Luna <Sofia21luna@gmail.com>
2020-09-01 15:14:43 -0400

    double click

--- js/index.js
+++ js/index.js
@@ -11,3 +11,10 @@ funBus.addEventListener('mouseenter', (e) => {
 funBus.addEventListener('mouseleave', () => {
     funBus.style.transform = "scale(1)";
   })
+
+// 3 -double click
+let adventureImg = document.querySelector('.img-content img')
+
+adventureImg.addEventListener('dblclick', (event) => {
+    event.target.src = "https://images.unsplash.com/photo-1515552726023-7125c8d07fb3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"
+})
