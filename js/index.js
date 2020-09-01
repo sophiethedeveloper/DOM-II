1021c9c40b789a385a6272c7bc92c75b7af2b292
sofia Luna <Sofia21luna@gmail.com>
2020-09-01 15:12:53 -0400

    mouse leave

--- js/index.js
+++ js/index.js
@@ -6,3 +6,8 @@ funBus.addEventListener('mouseenter', (e) => {
     funBus.style.transform = "scale(1.2)"
     funBus.style.transition = "all 0.3s"
   })
+
+// 2 -mouseleave
+funBus.addEventListener('mouseleave', () => {
+    funBus.style.transform = "scale(1)";
+  })
